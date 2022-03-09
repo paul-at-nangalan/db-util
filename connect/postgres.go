c4cc8d35616a2235589ed3f113e85ada700d1505
paul reed <paul@nesystemsasia.com>
2022-03-09 21:05:24 +0000

    add connection helper for postgres

---
+++ connect/postgres.go
@@ -0,0 +1,43 @@
+package connect
+
+import (
+	"database/sql"
+	"github.com/paul-at-nangalan/json-config/cfg"
+	"github.com/paul-at-nangalan/errorhandler/handlers"
+	"os"
+)
+
+type PostgresCfg struct{
+	Username string
+	Password string
+	Host string
+	Database string
+	CAFile string
+	Sslmode string
+
+	basedir string
+}
+
+func (p *PostgresCfg) Expand() {
+	p.Username = os.ExpandEnv(p.Username)
+	p.Password = os.ExpandEnv(p.Password)
+	p.Host = os.ExpandEnv(p.Host)
+	p.Database = os.ExpandEnv(p.Database)
+	p.CAFile = os.ExpandEnv(p.CAFile)
+}
+
+func Connect()*sql.DB{
+	postgrescfg := PostgresCfg{}
+	cfg.Read("postgres", &postgrescfg)
+	constr := `host=` + postgrescfg.Host + ` ` +
+		`dbname=`+ postgrescfg.Database + ` ` +
+		`user=` + postgrescfg.Username + ` ` +
+		`password=` + postgrescfg.Password + ` ` +
+		`sslmode=` + postgrescfg.Sslmode + ` ` +
+		`sslrootcert=` + postgrescfg.CAFile
+	//fmt.Println("Postgres params: ", constr)
+	db, err := sql.Open("postgres", constr)
+	handlers.PanicOnError(err)
+
+	return db
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/paul-at-nangalan/db-util
 
 go 1.14
 
-require github.com/paul-at-nangalan/errorhandler v0.0.0-20210525060727-cba369c8785e
+require (
+	github.com/paul-at-nangalan/errorhandler v0.0.0-20210525060727-cba369c8785e
+	github.com/paul-at-nangalan/json-config v0.0.0-20210525054146-58797ba49d12
+)
