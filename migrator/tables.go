9907528e603c8f9681639569159a7ffedcec67e3
Paul <paul@somewhere>
2024-11-26 16:45:35 +0800

    added postgres handling for alter col type

--- migrator/tables.go
+++ migrator/tables.go
@@ -9,39 +9,39 @@ import (
 
 type DbType string
 
-const(
-	DBTYPE_MYSQL DbType = "mysql"
+const (
+	DBTYPE_MYSQL    DbType = "mysql"
 	DBTYPE_POSTGRES DbType = "postgres" ////NOTE: this has not been thoroughly used/tested
 )
 
-type Migrator struct{
-	db *sql.DB
+type Migrator struct {
+	db            *sql.DB
 	migrationsmap map[string]bool
 
 	dbtype DbType
 }
 
-func NewMigrator(db *sql.DB, dbtype DbType)*Migrator{
+func NewMigrator(db *sql.DB, dbtype DbType) *Migrator {
 	createmigtable := `CREATE TABLE IF NOT EXISTS migrations (migration text)`
 	_, err := db.Exec(createmigtable)
 	handlers.PanicOnError(err)
 
 	migrator := &Migrator{
-		db: db,
+		db:     db,
 		dbtype: dbtype,
 	}
 	migrator.loadMigs()
 	return migrator
 }
 
-func (p *Migrator)loadMigs(){
+func (p *Migrator) loadMigs() {
 	loadmigs := `SELECT migration FROM migrations`
 	res, err := p.db.Query(loadmigs)
 	handlers.PanicOnError(err)
 	defer res.Close()
 
 	migrations := make(map[string]bool)
-	for res.Next(){
+	for res.Next() {
 		mig := ""
 		err := res.Scan(&mig)
 		handlers.PanicOnError(err)
@@ -50,11 +50,10 @@ func (p *Migrator)loadMigs(){
 	p.migrationsmap = migrations
 }
 
-
-//columns is a map of column name to specification
-/// Use Migrate to create a table
-func (p *Migrator)createTable(
-	name string, columns map[string]string, indexes []string, primarykeys []string){
+// columns is a map of column name to specification
+// / Use Migrate to create a table
+func (p *Migrator) createTable(
+	name string, columns map[string]string, indexes []string, primarykeys []string) {
 
 	stmt := "Create table If not Exists " + name + " ("
 	sep := ""
@@ -68,50 +67,48 @@ func (p *Migrator)createTable(
 			sep = ", "
 		}
 	}
-	stmt += sep + " Primary Key(";
+	stmt += sep + " Primary Key("
 	sep = ""
 	for _, primarykey := range primarykeys {
-		stmt += sep +  primarykey
+		stmt += sep + primarykey
 		sep = ", "
 	}
 	stmt += "))"
 	log.Println("Create table stmt: " + stmt)
 	_, err := p.db.Exec(stmt)
 	if err != nil {
-		log.Println("Failed to create " + name  + " table with error " + err.Error())
+		log.Println("Failed to create " + name + " table with error " + err.Error())
 		log.Panic("Failed to create table")
 	}
-	if p.dbtype != DBTYPE_MYSQL{
-		for _, index := range indexes{
-			p.CreateIndex("add-index-" + name + "-" + index,
-				             name, index)
+	if p.dbtype != DBTYPE_MYSQL {
+		for _, index := range indexes {
+			p.CreateIndex("add-index-"+name+"-"+index,
+				name, index)
 		}
 	}
 }
 
-func (p *Migrator)CreateIndex(migration string, table string, colname string){
+func (p *Migrator) CreateIndex(migration string, table string, colname string) {
 	if _, ok := p.migrationsmap[migration]; ok {
 		return
 	}
 	stmt := `CREATE INDEX ON ` + table + ` (` + colname + `)`
 	_, err := p.db.Exec(stmt)
 	if err != nil {
-		log.Println("Failed to create index " + migration  + " with error " + err.Error())
+		log.Println("Failed to create index " + migration + " with error " + err.Error())
 		log.Panic("Failed to create index")
 	}
 	p.markMigration(migration)
 }
 
-
-
-func (p *Migrator)AlterTableAdd(migration string, name string, columns map[string]string){
+func (p *Migrator) AlterTableAdd(migration string, name string, columns map[string]string) {
 	if _, ok := p.migrationsmap[migration]; ok {
 		return
 	}
 	stmt := "Alter table " + name + " add "
-	if p.dbtype == DBTYPE_MYSQL{
+	if p.dbtype == DBTYPE_MYSQL {
 		stmt += "("
-	}else if p.dbtype == DBTYPE_POSTGRES{
+	} else if p.dbtype == DBTYPE_POSTGRES {
 		stmt += " column "
 	}
 	sep := ""
@@ -124,13 +121,13 @@ func (p *Migrator)AlterTableAdd(migration string, name string, columns map[strin
 	}
 	_, err := p.db.Exec(stmt)
 	if err != nil {
-		log.Println("Failed to create " + name  + " table with error " + err.Error())
+		log.Println("Failed to create " + name + " table with error " + err.Error())
 		log.Panic("Failed to create table")
 	}
 	p.markMigration(migration)
 }
 
-func (p *Migrator)Migrate(migration string, tablename string, columns map[string]string, indexes []string, primarykeys []string){
+func (p *Migrator) Migrate(migration string, tablename string, columns map[string]string, indexes []string, primarykeys []string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		///this migration is new
 		p.createTable(tablename, columns, indexes, primarykeys)
@@ -138,31 +135,38 @@ func (p *Migrator)Migrate(migration string, tablename string, columns map[string
 	}
 }
 
-func (p *Migrator)markMigration(migration string){
-	_,err := p.db.Exec("Insert into migrations (migration) VALUES($1)", migration)
+func (p *Migrator) markMigration(migration string) {
+	_, err := p.db.Exec("Insert into migrations (migration) VALUES($1)", migration)
 	if err != nil {
-		log.Println("Failed to set migration " + migration  + " table with error " + err.Error())
+		log.Println("Failed to set migration " + migration + " table with error " + err.Error())
 		log.Panic("Failed to create table")
 	}
 	log.Println("Migrated " + migration)
 }
 
-func (p *Migrator)Alter(migration string, tablename string, columns map[string]string){
+func (p *Migrator) Alter(migration string, tablename string, columns map[string]string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		///this migration is new
 		p.AlterTableAdd(migration, tablename, columns)
 	}
 }
 
-func (p *Migrator)AlterColumnDef(migration string, tablename string, columns map[string]string){
+func (p *Migrator) AlterColumnDef(migration string, tablename string, columns map[string]string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		///this migration is new
+		modkeyword := "modify  column "
+		typekeyword := " "
+		if p.dbtype == DBTYPE_POSTGRES {
+			modkeyword = "alter column "
+			typekeyword = " TYPE "
+		}
 		for colname, coltype := range columns {
-			stmt := "Alter table " + tablename + " modify column "
-			stmt +=  colname + " " + coltype
+			stmt := "Alter table " + tablename + " " + modkeyword
+			stmt += colname + typekeyword + coltype
 			_, err := p.db.Exec(stmt)
 			if err != nil {
-				log.Println("Failed to alter " + tablename  + " table with error " + err.Error())
+				log.Println("Failed to alter "+tablename+" table with error "+err.Error(),
+					stmt)
 				log.Panic("Failed to create table")
 			}
 		}
@@ -171,8 +175,7 @@ func (p *Migrator)AlterColumnDef(migration string, tablename string, columns map
 	}
 }
 
-
-func (p *Migrator)AlterPrimes(migration string, tablename string, primarykeys []string){
+func (p *Migrator) AlterPrimes(migration string, tablename string, primarykeys []string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		sep := ""
 		allkeys := ""
@@ -180,21 +183,21 @@ func (p *Migrator)AlterPrimes(migration string, tablename string, primarykeys []
 			allkeys += sep + key
 			sep = ", "
 		}
-		if p.dbtype == DBTYPE_MYSQL{
+		if p.dbtype == DBTYPE_MYSQL {
 			_, err := p.db.Exec("alter table " + tablename + " drop primary key, add primary key(" + allkeys + ")")
 			if err != nil {
 				log.Println("Failed to alter primary keys ", err)
 				log.Panic("Failed to alter primary keys")
 			}
-		}else{
+		} else {
 			_, err := p.db.Exec("alter table " + tablename + " DROP CONSTRAINT " +
 				tablename + "_pkey")
-			if err != nil{
+			if err != nil {
 				log.Panicln("Failed to alter primary key ", err)
 			}
 			_, err = p.db.Exec("alter table " + tablename + " ADD PRIMARY KEY (" +
 				allkeys + ")")
-			if err != nil{
+			if err != nil {
 				log.Panicln("Failed to alter primary key ", err)
 			}
 		}
@@ -203,7 +206,7 @@ func (p *Migrator)AlterPrimes(migration string, tablename string, primarykeys []
 	}
 }
 
-func (p *Migrator)DropPrimes(migration string, tablename string){
+func (p *Migrator) DropPrimes(migration string, tablename string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		_, err := p.db.Exec("alter table " + tablename + " drop primary key")
 		if err != nil {
@@ -214,7 +217,7 @@ func (p *Migrator)DropPrimes(migration string, tablename string){
 	}
 }
 
-func (p *Migrator)AlterIndexes(migration string, tablename string, indexes []string){
+func (p *Migrator) AlterIndexes(migration string, tablename string, indexes []string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		sep := ""
 		allindexes := ""
@@ -231,23 +234,23 @@ func (p *Migrator)AlterIndexes(migration string, tablename string, indexes []str
 	}
 }
 
-func (p *Migrator)AddUniqueConstrint(migration string, table string, col string){
+func (p *Migrator) AddUniqueConstrint(migration string, table string, col string) {
 
-	if p.dbtype == DBTYPE_MYSQL{
+	if p.dbtype == DBTYPE_MYSQL {
 		log.Panicln("Add unique constraint: Not yet supported for mysql")
 	}
 	createindx := `CREATE UNIQUE INDEX CONCURRENTLY ` + table + `_` +
-					col +
-                    ` ON equipment (equip_id)`
-	p.MigrateRaw(migration + "careat-index", createindx)
+		col +
+		` ON equipment (equip_id)`
+	p.MigrateRaw(migration+"careat-index", createindx)
 
 	addconstraint := `ALTER TABLE ` + table +
-                    ` ADD CONSTRAINT unique_` + col +
-                    ` UNIQUE USING INDEX ` + table + col
+		` ADD CONSTRAINT unique_` + col +
+		` UNIQUE USING INDEX ` + table + col
 	p.MigrateRaw(migration, addconstraint)
 }
 
-func (p *Migrator)MigrateRaw(migration string, qry string){
+func (p *Migrator) MigrateRaw(migration string, qry string) {
 	if _, ok := p.migrationsmap[migration]; !ok {
 		_, err := p.db.Exec(qry)
 		if err != nil {
@@ -257,9 +260,9 @@ func (p *Migrator)MigrateRaw(migration string, qry string){
 	}
 }
 
-func (p *Migrator)AddAuditTrigger(
+func (p *Migrator) AddAuditTrigger(
 	migration string, origtable string, cols map[string]string, createaudittable bool,
-	audittable string){
+	audittable string) {
 
 	///older code may have differently named the audittable
 	if audittable == "" {
@@ -268,35 +271,32 @@ func (p *Migrator)AddAuditTrigger(
 	}
 	triggername := "audit_" + strings.ToLower(origtable)
 
-	if createaudittable {///if audit table exists ... don't recreate ... you'll need to alter it explicitly if required
-		qry := `CREATE TABLE ` + audittable + ` LIKE ` + origtable;
+	if createaudittable { ///if audit table exists ... don't recreate ... you'll need to alter it explicitly if required
+		qry := `CREATE TABLE ` + audittable + ` LIKE ` + origtable
 		p.MigrateRaw(migration+"create_audit_table", qry)
 
-		qry = `ALTER TABLE ` + audittable + ` ADD COLUMN DateOfChange TIMESTAMP DEFAULT NOW()`;
+		qry = `ALTER TABLE ` + audittable + ` ADD COLUMN DateOfChange TIMESTAMP DEFAULT NOW()`
 		p.MigrateRaw(migration+"alter_audit_table", qry)
 
-		qry = `ALTER TABLE ` + audittable + ` DROP PRIMARY KEY`;
+		qry = `ALTER TABLE ` + audittable + ` DROP PRIMARY KEY`
 		p.MigrateRaw(migration+"drop_primes", qry)
 
-		qry = `ALTER TABLE ` + audittable + ` ADD COLUMN idindx BIGINT AUTO_INCREMENT PRIMARY KEY`;
+		qry = `ALTER TABLE ` + audittable + ` ADD COLUMN idindx BIGINT AUTO_INCREMENT PRIMARY KEY`
 		p.MigrateRaw(migration+"add_idindx", qry)
 	}
 	qry := `CREATE TRIGGER ` + triggername + ` BEFORE UPDATE ON ` + origtable +
 		` FOR EACH ROW
     			INSERT INTO ` + audittable + ` SET `
 	sep := ""
-	for colname, _ := range cols{
+	for colname, _ := range cols {
 		qry += sep + colname + "=OLD." + colname
 		sep = ","
 	}
 	p.MigrateRaw(migration, qry)
 }
 
-func (p *Migrator)DropAuditTrigger(migration string, origtable string){
+func (p *Migrator) DropAuditTrigger(migration string, origtable string) {
 	triggername := "audit_" + strings.ToLower(origtable)
 	qry := `DROP TRIGGER ` + triggername
-	p.MigrateRaw(migration + "drop-trigger", qry)
+	p.MigrateRaw(migration+"drop-trigger", qry)
 }
-
-
-
